73289c7e382b97ce7ee41a9812a904cd63ea94a4
Maksut Kaya <mkst.kya@gmail.com>
2018-03-06 17:49:42 +0300

    Initial commit

---
+++ ImageUpload/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/genel.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ImageUpload.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .auto-style1 {
+            width: 82px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <table class="auto-style1">
+
+                <asp:Label ID="lblUyari" runat="server" Text="" ForeColor="Red"></asp:Label>
+
+        <tr>
+            <td class="auto-style2">
+                <asp:Image ID="imgSmall" runat="server" Style="width: 75px; height: 75px;" />
+                &nbsp;</td>
+            <td>
+                <asp:FileUpload ID="flSmallPhoto" runat="server" Width="249px" /></td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style2">&nbsp;</td>
+            <td style="text-align: left; vertical-align: top; width: 250px;">
+                <asp:Button Style="width: 100%" ID="BtnEkle" runat="server" Text="Ekle" OnClick="BtnEkle_Click" />
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+
+    </table>
+
+
+</asp:Content>
---
+++ ImageUpload/Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ImageUpload
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnEkle_Click(object sender, EventArgs e)
+        {
+            fileName = Path.GetFileName(flSmallPhoto.FileName);
+            if (flSmallPhoto.HasFile)
+            {
+                if (flSmallPhoto.PostedFile.ContentType == "image/jpeg" || flSmallPhoto.PostedFile.ContentType == "image/png")
+                {
+                    if (flSmallPhoto.PostedFile.ContentLength < 1024000)
+                    {
+                        flSmallPhoto.PostedFile.SaveAs(Server.MapPath("images/") + fileName.ToString());
+                        lblUyari.Text = "";
+                    }
+                    else
+                    {
+                        lblUyari.Text = "Resim 100 kb'dan büyüktür...";
+                    }
+                }
+                else
+                {
+                    lblUyari.Text = "Resim .jpeg/.png dosyası olmalıdır";
+                }
+            }
+
+
+            if (imgSmall.ImageUrl != "images/No_Image_Small.jpg" & imgSmall.ImageUrl != "" && flSmallPhoto.FileName != "")
+            {
+                File.Delete(Server.MapPath(imgSmall.ImageUrl));
+            }
+            if (fileName == "")
+            {
+                imgSmall.ImageUrl = "images/No_Image_Small.jpg";
+            }
+            else
+            {
+                imgSmall.ImageUrl = "images/" + fileName.ToString();
+            }
+
+        }
+
+        public string fileName { get; set; }
+        public byte[] filePath { get; set; }
+        public Image SmallPhoto { get; set; }
+
+    }
+}
